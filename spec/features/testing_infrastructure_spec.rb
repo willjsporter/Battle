78289da025c1f9880feea0672c63c8c1794d0b85
Will Porter <willjsporter@hotmail.co.uk>
2017-08-16 21:33:58 +0100

    redone rspec second test

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,20 @@
 require_relative '../../app.rb'
 
   feature 'Testing infrastructure' do
-    scenario 'Can run app and check page content' do
+
+    before(:each) do
       visit('/')
+    end
+    scenario 'Can run app and check page content' do
       expect(page).to have_content 'Testing infrastructure working!'
     end
+
+    scenario "Can enter names and see them on the screen" do
+      fill_in('player1', with: 'Nick')
+      fill_in('player2', with: 'Will')
+      click_button('Submit')
+      expect(page).to have_content 'Nick'
+      expect(page).to have_content 'Will'
+    end
+
   end
